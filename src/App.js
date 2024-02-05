9e258f71189114b78372086e1f63116cb1de04bd
Your Name <olivertravis554@gmail.com>
2024-02-04 22:40:11 -0500

    create listings starter code

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Category from './pages/Category';
+import CreateListing from './pages/CreateListing';
 
 function App() {
 	return (
@@ -47,6 +48,10 @@ function App() {
 					<Route
 						path='/sign-up'
 						element={<SignUp />}
+					/>
+					<Route
+						path='/create-listing'
+						element={<CreateListing />}
 					/>
 				</Routes>
 				<Navbar />
